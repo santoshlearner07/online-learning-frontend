f3d58fa235411de0bc1d29e3541ebec5641921aa
Santosh Nandiyawar <nandiyawarsantosh.0719@gmail.com>
2026-01-17 22:01:46 +0000

    Updated:- vercel file

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,5 @@
 // export const baseURL = 'http://localhost:5000/api'
-export const baseURL = 'https://itb-learning.onrender.com/'
+export const baseURL = 'https://itb-learning.onrender.com/api'
 
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
